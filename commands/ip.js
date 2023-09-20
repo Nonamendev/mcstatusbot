790526d72b8dbd0efe93e164a47f8bc0c0c6613e
Nonamendev <143304403+Nonamendev@users.noreply.github.com>
2023-09-20 12:32:07 +0530

    Create ip.js

---
+++ commands/ip.js
@@ -0,0 +1,14 @@
+'use strict';
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { embedColor } from '../functions/sendMessage.js';
+
+export const data = new SlashCommandBuilder().setName('iP').setDescription('IP of SnowMc');
+
+export async function execute(interaction) {
+	const helpEmbed = new EmbedBuilder().setTitle('SnowMc IP☄️').setColor(embedColor).addFields(
+		{
+			name: 'SnowMc ☄️',
+			value: 'Name : **Snow Mc**,iP : **play.snowmc.fun**,Port : **19132**'
+		},
+	await interaction.editReply({ embeds: [helpEmbed], ephemeral: true });
+}
